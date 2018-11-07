222350b7dfed8ffa457f71d6287ff08d8a397bc4
swapyapp <43712240+swapyapp@users.noreply.github.com>
2018-11-07 19:15:21 +0200

    what to do here Jimmy

--- app/src/main/java/com/app/muhammadgamal/swapy/Activities/ReceivedSwapRequest.java
+++ app/src/main/java/com/app/muhammadgamal/swapy/Activities/ReceivedSwapRequest.java
@@ -11,5 +11,11 @@ public class ReceivedSwapRequest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_received_swap_request);
+
+        /* TODO : 1- get the swap informaton from an Intent
+                  2- set the Fragment that will contain the swap requests
+                  3- set the adapter that will represent the swaps in ListView
+                  4- Finally set a method allow the user to swipe other swaps he have created     
+        */
     }
 }
