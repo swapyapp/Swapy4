f517f004ca4a445cb6eed0762330531743c4a924
swapyapp <43712240+swapyapp@users.noreply.github.com>
2018-11-24 20:42:06 +0200

    l

--- app/src/main/java/com/app/muhammadgamal/swapy/Activities/SwapOffCreationActivity.java
+++ app/src/main/java/com/app/muhammadgamal/swapy/Activities/SwapOffCreationActivity.java
@@ -3,6 +3,8 @@ package com.app.muhammadgamal.swapy.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.app.muhammadgamal.swapy.R;
+
 public class SwapOffCreationActivity extends AppCompatActivity {
 
     @Override
