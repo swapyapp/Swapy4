a3fb300e75ef819668038f0c0e1e36dcd3b8f16c
swapyapp <43712240+swapyapp@users.noreply.github.com>
2018-12-01 00:15:56 +0200

    TabLayout code

--- app/src/main/java/com/app/muhammadgamal/swapy/Fragments/HomeFragment.java
+++ app/src/main/java/com/app/muhammadgamal/swapy/Fragments/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -29,6 +30,9 @@ public class HomeFragment extends Fragment {
         ViewPager vpPager = (ViewPager) rootView.findViewById(R.id.vpPager);
         adapterViewPager = new MyPagerAdapter(getChildFragmentManager());
         vpPager.setAdapter(adapterViewPager);
+
+        TabLayout tabLayout = rootView.findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(vpPager);
 
         if (savedInstanceState == null) {
             getActivity().getSupportFragmentManager().
