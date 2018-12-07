6532c2dc74f9c3b3f379122e89c8e427951eac2d
swapyapp <43712240+swapyapp@users.noreply.github.com>
2018-12-07 05:49:46 +0200

    creating account sittings

---
+++ app/src/main/java/com/app/muhammadgamal/swapy/Activities/AccountSittings.java
@@ -0,0 +1,15 @@
+package com.app.muhammadgamal.swapy.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.muhammadgamal.swapy.R;
+
+public class AccountSittings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_sittings);
+    }
+}
