0181d72994adbe883087ee312070372022aea5a8
swapyapp <43712240+swapyapp@users.noreply.github.com>
2019-03-01 22:32:10 +0200

    added notification reciever

---
+++ app/src/main/java/com/app/muhammadgamal/swapy/Notifications/NotificationRequists.java
@@ -0,0 +1,29 @@
+package com.app.muhammadgamal.swapy.Notifications;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.app.muhammadgamal.swapy.Fragments.ReceivedSwapsFragment;
+import com.app.muhammadgamal.swapy.R;
+
+public class NotificationRequists extends AppCompatActivity {
+
+    private Button openNotificationTab;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_requists);
+        getActionBar().hide();
+
+        openNotificationTab = (Button)findViewById(R.id.open_notification_btn);
+        openNotificationTab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NotificationRequists.this, ReceivedSwapsFragment.class);
+            }
+        });
+    }
+}
