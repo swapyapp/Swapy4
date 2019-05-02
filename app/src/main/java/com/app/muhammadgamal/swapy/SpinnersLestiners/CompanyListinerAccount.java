6191ef4b85e875f584d99a9c03840f6edccea06a
Jimmy <muhammadjimmy7@gmail.com>
2019-05-02 17:29:02 +0200

    spinners

--- app/src/main/java/com/app/muhammadgamal/swapy/SpinnersLestiners/CompanyListinerAccount.java
+++ app/src/main/java/com/app/muhammadgamal/swapy/SpinnersLestiners/CompanyListinerAccount.java
@@ -18,32 +18,9 @@ public class CompanyListinerAccount implements AdapterView.OnItemSelectedListene
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         if (parent.getItemAtPosition(position).equals("Choose company")) {
             company = parent.getItemAtPosition(position).toString();
-            ChangeCompany.arrayBranch = R.array.branch;
-            //do nothing
-            ChangeCompany.COMPANY_CHOSEN = 1;
-            ChangeCompany.spinnerCompanyBranchRaya.setVisibility(View.GONE);
-            ChangeCompany.spinnerCompanyBranchOrange.setVisibility(View.GONE);
-            ChangeCompany.spinnerCompanyBranchVodafone.setVisibility(View.GONE);
         } else {
             ChangeCompany.COMPANY_CHOSEN = 0;
             company = parent.getItemAtPosition(position).toString();
-            if (company.equals("Raya")) {
-                ChangeCompany.spinnerCompanyBranchRaya.setVisibility(View.VISIBLE);
-                ChangeCompany.spinnerCompanyBranchOrange.setVisibility(View.GONE);
-                ChangeCompany.spinnerCompanyBranchVodafone.setVisibility(View.GONE);
-            } else if (company.equals("Vodafone")) {
-                ChangeCompany.spinnerCompanyBranchRaya.setVisibility(View.GONE);
-                ChangeCompany.spinnerCompanyBranchOrange.setVisibility(View.GONE);
-                ChangeCompany.spinnerCompanyBranchVodafone.setVisibility(View.VISIBLE);
-            } else if (company.equals("Orange")) {
-                ChangeCompany.spinnerCompanyBranchRaya.setVisibility(View.GONE);
-                ChangeCompany.spinnerCompanyBranchOrange.setVisibility(View.VISIBLE);
-                ChangeCompany.spinnerCompanyBranchVodafone.setVisibility(View.GONE);
-            }else {
-                ChangeCompany.spinnerCompanyBranchRaya.setVisibility(View.GONE);
-                ChangeCompany.spinnerCompanyBranchOrange.setVisibility(View.GONE);
-                ChangeCompany.spinnerCompanyBranchVodafone.setVisibility(View.GONE);
-            }
         }
     }
 
